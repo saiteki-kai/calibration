27d71852e0b7dcb33f26e9c539f94567567f9e27
saiteki-kai <giuseppe.magazu@gmail.com>
2025-07-13 03:23:40 +0200

    fix: correct model name

--- run_experiments.sh
+++ run_experiments.sh
@@ -9,7 +9,7 @@ hf_name_to_path() {
 }
 
 dataset="PKU-Alignment/Beavertails"
-dev_split="330k_train" # (10% of 330k)
+dev_split="330k_train" # (use 10% of 330k)
 eval_split="330k_test"
 
 taxonomy="beavertails"
@@ -17,12 +17,12 @@ descriptions="False"
 
 models=(
     "meta-llama/Llama-Guard-3-8B"
-    "saiteki-kai/Llama-Guard-3-8B-Lora"
+    "saiteki-kai/QA-Llama-Guard-3-8B"
 )
 
 ece_bins=15
 plot_bins=10
-sample_size=1000
+# sample_size=2000 # (for debugging, use a smaller sample size)
 
 evaluation_path="results/evaluation/"
 tuning_path="results/tuning/"
@@ -30,7 +30,6 @@ comparison_path="results/comparison/"
 
 for model in "${models[@]}"; do
     # 1. Tuning parametrized calibration methods
-
     python scripts/tuning.py --model "${model}" \
         --dataset-name "${dataset}" \
         --split "${dev_split}" \
@@ -39,13 +38,12 @@ for model in "${models[@]}"; do
         --ece-bins "${ece_bins}" \
         --output-path "${tuning_path}$(hf_name_to_path "$model")/"
 
+    # Get the best hyperparameters from tuning results
     temperature=$(jq <"${tuning_path}$(hf_name_to_path "$model")/temperature.json" -r '.best')
     gamma=$(jq <"${tuning_path}$(hf_name_to_path "$model")/gamma.json" -r '.best')
 
     # 2. Evaluate uncalibrated and calibrated models
-
     python scripts/eval.py --model "${model}" \
-        --sample-size ${sample_size} \
         --dataset-name "${dataset}" \
         --split "${eval_split}" \
         --taxonomy "${taxonomy}" \
@@ -54,17 +52,17 @@ for model in "${models[@]}"; do
         --temperature "${temperature}" \
         --gamma "${gamma}" \
         --output-path "${evaluation_path}$(hf_name_to_path "$model")/"
+        # --sample-size ${sample_size} \
 done
 
 # 3. Compare models through metrics and plots
-
 python scripts/plots.py --models "${models[@]}" \
     --dataset-name "${dataset}" \
-    --sample-size ${sample_size} \
     --split "${eval_split}" \
     --plot-bins "${plot_bins}" \
     --input-path "${evaluation_path}" \
     --output-path "${comparison_path}"
+    # --sample-size ${sample_size} \
 
 python scripts/metrics.py \
     --models "${models[@]}" \
